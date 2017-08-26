2aa20a45dea52c58aa5cae4dbe948171e9bc5750
Gavin Nitta <gshigeto@gmail.com>
2017-08-25 19:28:22 -0600

    Add environment model

---
+++ src/environments/environment.model.ts
@@ -0,0 +1,12 @@
+export interface Environment {
+  firebase: Firebase
+}
+
+export interface Firebase {
+  apiKey: string;
+  authDomain: string;
+  databaseURL: string;
+  projectId: string;
+  storageBucket: string;
+  messagingSenderId: string;
+}
